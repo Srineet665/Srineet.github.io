01bfa2b5af594db36a057a4b64acc7899c292d17
Srineet665 <100183328+Srineet665@users.noreply.github.com>
2025-08-08 01:54:06 -0500

    Rebuild site with TypeScript and Sass

---
+++ dist/main.js
@@ -0,0 +1,19 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('toggle-dark');
+    toggle === null || toggle === void 0 ? void 0 : toggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+    const intro = document.getElementById('intro');
+    const text = "Hi, I'm Srineet.";
+    let i = 0;
+    function type() {
+        if (intro) {
+            intro.textContent = text.slice(0, i++);
+            if (i <= text.length) {
+                setTimeout(type, 150);
+            }
+        }
+    }
+    type();
+});
